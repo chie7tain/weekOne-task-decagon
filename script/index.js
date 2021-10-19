95428cd84afb66c9ce7fbf43a05a625ac9152b15
chei7tain <fredrickokwuobi@gmail.com>
2021-10-19 19:28:33 +0100

    add loader

--- script/index.js
+++ script/index.js
@@ -5,115 +5,172 @@
 // module.exports = {main}
 
 //
+// DOM Elements
 const charactersDOM = document.querySelector(".all-characters-tile-display");
 const singleCharacterDOM = document.querySelector(
   ".single-character-details-display"
 );
+const backBtnContainer = document.querySelector(".back-button-container");
+const backBtn = document.querySelector(".back-btn");
+const spinner = document.querySelector(".spinner-wrapper");
+const pageBtnContainer = document.querySelector(".page-btn-container");
+const previousBtn = pageBtnContainer.querySelector("#prev-Btn");
+const nextBtn = pageBtnContainer.querySelector("#next-Btn");
 
-const url = `https://swapi.dev/api/people`;
+// end of DOM Elements
 
-// fetch data
-const getCharacters = async (url) => {
-  // we put the logic throught the try catch block
-  // in order to handle errors
-  try {
-    //  using fetch and an IIFE we get the data
-    const data = await (await fetch(url)).json();
-    // const imageData = await (await fetch(imageUrl)).json();
-    // console.log(imageData);
-    let { count, next, previous, results } = data;
-    // using map we extract just what we need {name height and gender}
-    results = results.map((character) => {
-      let { name, height, gender, url } = character;
-      return { name, height, gender, url };
+let urlHolder = {
+  url: `https://swapi.dev/api/people`,
+};
+// DATA class
+// UI Class
+class Data {
+  // fetch data
+  static getCharacters = async (url) => {
+    // we put the logic throught the try catch block
+    // in order to handle errors
+    try {
+      //  using fetch and an IIFE we get the data
+      const data = await (await fetch(url)).json();
+
+      // const imageData = await (await fetch(imageUrl)).json();
+      // console.log(imageData);
+      let { count, next, previous, results } = data;
+      console.log(data);
+      // using map we extract just what we need {name height and gender}
+      results = results.map((character) => {
+        let { name, height, gender, url } = character;
+        return { name, height, gender, url };
+      });
+      return { count, next, previous, results };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+// UI Class
+class UI {
+  // display all characters
+  static displayCharacters = (data, display) => {
+    let charactersString = "";
+    let { results } = data;
+    results.forEach((character) => {
+      charactersString += `
+      <div class="character-tile">
+            <div class="character-img-container">
+              <img src="./assets/characters/${parseInt(
+                Helper.getIdFromUrl(character.url)
+              )}.jpg" alt=${character.name} class="character-img">
+            </div>
+            <div class="mini character-name-container">
+                <h3 class="character-name" data-name=${character.name}>
+                    ${character.name}
+                </h3>
+            </div>
+        </div>
+      `;
     });
-    return { count, next, previous, results };
-  } catch (error) {
-    console.log(error);
-  }
-};
-// STORAGE things
-// save data
-const saveCharacters = ({ results }) => {
-  localStorage.setItem("starWarsCharacters", JSON.stringify(results));
-};
-// getCharacters
-const retriveCharacter = (nameTag) => {
-  let characters = JSON.parse(localStorage.getItem("starWarsCharacters"));
-  return characters.find((character) => character.name.includes(nameTag));
-};
-
-// UI things
-// display all characters
-const displayCharacters = (data, display) => {
-  let charactersString = "";
-  let { results } = data;
-  results.forEach((character) => {
-    charactersString += `
-    <div class="character-tile">
-          <div class="character-img-container">
-            <img src="./assets/characters/${parseInt(
-              getIdFromUrl(character.url)
-            )}.jpg" alt=${character.name} class="character-img">
-          </div>
-          <div class="mini character-name-container">
-              <h3 class="character-name" data-name=${character.name}>
-                  ${character.name}
-              </h3>
-          </div>
-      </div>
-    `;
-  });
-  display.innerHTML = charactersString;
-};
-// get the id for the particular star wars character from the url
-var getIdFromUrl = function (value) {
-  var id = value.match(/([0-9])+/g);
-  id = id[0];
-  return id;
-};
-const getCharacterNames = () => {
-  let names = [...document.querySelectorAll(".character-name")];
-  names.forEach((name) => {
-    let nameTag = name.dataset.name;
-    name.addEventListener("click", (e) => {
-      // get characters from storage
-      let character = retriveCharacter(nameTag);
-      displaySingleCharacter(character, singleCharacterDOM);
-    });
-  });
-};
-// display single data
-const displaySingleCharacter = (character, display) => {
-  let characterString = `<div class="character-img-container-info">
-    <img src="./assets/characters/${parseInt(
-      getIdFromUrl(character.url)
-    )}.jpg" alt=${character.name} class="character-img">
-  </div>
-  <div class="full-info-container">
-    <div class="full character-name-container">
-      <h2 class="character-name-info">
-        ${character.name}
-      </h2>
+    display.innerHTML = charactersString;
+  };
+  // display single character
+  static displaySingleCharacter = (character, display) => {
+    let characterString = `<div class="character-img-container-info">
+      <img src="./assets/characters/${parseInt(
+        Helper.getIdFromUrl(character.url)
+      )}.jpg" alt=${character.name} class="character-img">
     </div>
-    <ul class="mini-info">
-      <li class="info">Gender: ${character.gender}</li>
-      <li class="info">Height: ${character.height}</li>
-    </ul>
-  </div>`;
-  display.innerHTML = characterString;
-};
+    <div class="full-info-container">
+      <div class="full character-name-container">
+        <h2 class="character-name-info">
+          ${character.name}
+        </h2>
+      </div>
+      <ul class="mini-info">
+        <li class="info">Gender: ${character.gender}</li>
+        <li class="info">Height: ${character.height}</li>
+      </ul>
+    </div>`;
+    display.innerHTML = characterString;
+  };
+  // =====
+  static populateSingleCharacter = () => {
+    let names = [...document.querySelectorAll(".character-name")];
+    let character = "";
+    names.forEach((name) => {
+      let nameTag = name.dataset.name;
+      name.addEventListener("click", (e) => {
+        // get characters from storage
+        backBtnContainer.classList.add("show-item");
+        pageBtnContainer.classList.add("hide-item");
+        character = Storage.retriveCharacter(nameTag);
+        UI.displaySingleCharacter(character, singleCharacterDOM);
+        UI.updateCharacterDom(charactersDOM, singleCharacterDOM);
+      });
+    });
+  };
+  // =====
+  static updateCharacterDom = (targetDom, replacementDom) => {
+    targetDom.classList.add("hide-item");
+    replacementDom.classList.remove("hide-item");
+
+    backBtn.addEventListener("click", () => {
+      pageBtnContainer.classList.remove("hide-item");
+      targetDom.classList.remove("hide-item");
+      replacementDom.classList.add("hide-item");
+      backBtnContainer.classList.remove("show-item");
+      backBtnContainer.classList.remove("show-item");
+    });
+  };
+  // ======
+  static paginateCharacters = (charactersData, charactersDOM) => {
+    let {
+      count,
+      next: nextUrl,
+      previous: previousUrl,
+      results,
+    } = charactersData;
+    console.log(nextUrl);
+    nextBtn.addEventListener("click", () => {
+      mainUrl = nextUrl;
+    });
+  };
+}
+
+// STORAGE things
+class Storage {
+  // save data
+  static saveCharacters = ({ results }) => {
+    localStorage.setItem("starWarsCharacters", JSON.stringify(results));
+  };
+  // getCharacters
+  static retriveCharacter = (nameTag) => {
+    let characters = JSON.parse(localStorage.getItem("starWarsCharacters"));
+    return characters.find((character) => character.name.includes(nameTag));
+  };
+}
+
+class Helper {
+  // get the id for the particular star wars character from the url
+  static getIdFromUrl = (value) => {
+    let id = value.match(/([0-9])+/g);
+    id = id[0];
+    return id;
+  };
+}
+
 // PAGINATION things
 // paginate data
-
+console.log(urlHolder.url);
 // setup app
 document.addEventListener("DOMContentLoaded", () => {
-  getCharacters(url)
-    .then((characters) => {
-      displayCharacters(characters, charactersDOM);
-      saveCharacters(characters);
+  Data.getCharacters(urlHolder.url)
+    .then((charactersData) => {
+      spinner.classList.add("hide-item");
+      UI.displayCharacters(charactersData, charactersDOM);
+      UI.paginateCharacters(charactersData, charactersDOM);
+      Storage.saveCharacters(charactersData);
     })
     .then(() => {
-      getCharacterNames();
+      UI.populateSingleCharacter();
     });
 });
